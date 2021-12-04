fc276c5c16492853b19609e6f2bee1d345257873
Daniel Podwysocki <daniel@dapo.tech>
2021-12-04 18:33:03 +0100

    feat: add xardas to the private network

--- hetzner-prod/instances.tf
+++ hetzner-prod/instances.tf
@@ -1,8 +1,14 @@
-resource "hcloud_server" "kube01" {
+resource "hcloud_server" "xardas" {
   name        = "xardas"
   image       = "rocky-8"
   server_type = "cx11"
   location    = "fsn1"
   ssh_keys    = ["daniel@Daniels-MBP"]
   
+  network {
+    network_id = hcloud_network.hzcloud-network.id
+    ip         = "10.0.0.1"
+
+  }
+
 }
