943d75c5125c38000393193f6c7e5e2e8321f641
Daniel Podwysocki <daniel@dapo.tech>
2021-12-05 17:44:00 +0100

    feat: create the docker01 instance

--- hetzner-prod/instances.tf
+++ hetzner-prod/instances.tf
@@ -28,3 +28,27 @@ resource "hcloud_server" "xardas" {
 }
 
 
+resource "hcloud_server" "docker01" {
+  name        = "docker01"
+  image       = "rocky-8"
+  server_type = "cx11"
+  location    = "fsn1"
+  ssh_keys    = ["daniel@Daniels-MBP"]
+  
+
+  network {
+    network_id = hcloud_network.hzcloud-network.id
+    ip         = "10.0.0.10"
+
+  }
+  firewall_ids = [
+    hcloud_firewall.allow-ssh.id,
+    hcloud_firewall.allow-consul.id,
+  ]
+
+  depends_on = [
+    hcloud_network_subnet.hzcloud-network-subnet-1
+  ]
+
+}
+
