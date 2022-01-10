43c1b2884c055288666b0a2d856d1b662debc560
Daniel Podwysocki <daniel@dapo.tech>
2022-01-10 06:30:34 +0100

    feat: define the Terraform cloud backend to switch to local execution

--- hetzner-prod/main.tf
+++ hetzner-prod/main.tf
@@ -2,13 +2,22 @@ terraform {
   required_version = ">= 1.0.11"
   required_providers {
     hcloud = {
-      source = "hetznercloud/hcloud"
+      source  = "hetznercloud/hcloud"
       version = "1.32.1"
     }
   }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "dapo_tech"
+
+    workspaces {
+      name = "hetzner-prod"
+    }
+  }
+
 }
 
-variable hcloud_token {
+variable "hcloud_token" {
   type        = string
   sensitive   = true
   description = "The Hetzner Cloud API token"
