f81b7f9b855965d311e8dfaaf9ec7a8efdb2694c
Daniel Podwysocki <daniel@dapo.tech>
2022-01-23 17:22:15 +0100

    feat: tag the instances with their names

--- oracle-prod/instances.tf
+++ oracle-prod/instances.tf
@@ -20,5 +20,35 @@ resource "oci_core_instance" "pyrokar" {
   metadata = {
     ssh_authorized_keys = local.ssh_public_key
   }
+
+  freeform_tags = {"name"= "pyrokar"}
+
+}
+
+resource "oci_core_instance" "serpentes" {
+  display_name        = "serpentes"
+  availability_domain = oci_core_subnet.production-a.availability_domain
+  compartment_id      = oci_identity_compartment.production.id
+  shape               = "VM.Standard.E2.1.Micro"
+  agent_config {
+    are_all_plugins_disabled = true
+  }
+
+  create_vnic_details {
+    subnet_id = oci_core_subnet.production-a.id
+    nsg_ids = [
+      oci_core_network_security_group.allow_ssh.id,
+    ]
+  }
+  source_details {
+    source_type = "image"
+    source_id   = var.linux_generic_image_id
+  }
+  metadata = {
+    ssh_authorized_keys = local.ssh_public_key
+  }
+
+  freeform_tags = {"name"= "serpentes"}
+
 }
 
