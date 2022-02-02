f36a8e5cb2646f22f35f87de453418a9bcaed00e
Daniel Podwysocki <daniel@dapo.tech>
2022-02-02 21:36:43 +0100

    style: terraform fmt

--- hetzner-prod/network.tf
+++ hetzner-prod/network.tf
@@ -1,6 +1,6 @@
 resource "hcloud_network" "hzcloud-network" {
-  name         = "hzcloud-network"
-  ip_range     = "10.0.0.0/16"
+  name     = "hzcloud-network"
+  ip_range = "10.0.0.0/16"
 }
 
 resource "hcloud_network_subnet" "hzcloud-network-subnet-1" {
