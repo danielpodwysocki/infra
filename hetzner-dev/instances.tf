9dac982c7c606b9122d765f4b85928684bcd6d89
danielpodwysocki <daniel@dapo.tech>
2022-06-08 07:46:24 +0200

    feat: create a cx11 instance in the dev environment

---
+++ hetzner-dev/instances.tf
@@ -0,0 +1,23 @@
+resource "hcloud_server" "xardas" {
+  name        = "xardas"
+  image       = "rocky-8"
+  server_type = "cx11"
+  location    = "fsn1"
+  ssh_keys    = ["daniel_mbp"]
+
+
+  network {
+    network_id = hcloud_network.hcloud-network.id
+    ip         = "172.21.0.10"
+  }
+
+  firewall_ids = [
+    hcloud_firewall.allow-ssh.id,
+  ]
+
+  depends_on = [
+    hcloud_network_subnet.hcloud-network-subnet-1,
+  ]
+
+}
+
