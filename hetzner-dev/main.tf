c13272978e4a9e87a2e675bc1c4450bb4cdb8d0a
danielpodwysocki <daniel@dapo.tech>
2022-06-08 07:54:44 +0200

    store the state and run status remotely on Terraform cloud

--- hetzner-dev/main.tf
+++ hetzner-dev/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "1.33.2"
     }
   }
+  cloud {
+    organization = "dapo_tech"
+
+    workspaces {
+      name = "hetzner-dev"
+    }
+  }
 }
 
 variable "hcloud_token" {
