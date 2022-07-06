cf2cc5bc80d945f06f94356924f77f320227f571
danielpodwysocki <daniel@dapo.tech>
2022-07-07 00:26:51 +0200

    create a new instance to test consul

--- hetzner-dev/instances.tf
+++ hetzner-dev/instances.tf
@@ -24,3 +24,29 @@ resource "hcloud_server" "xardas" {
 
 }
 
+resource "hcloud_server" "lester" {
+  name        = "lester"
+  image       = "rocky-8"
+  server_type = "cx11"
+  location    = "fsn1"
+  ssh_keys    = ["daniel_mbp"]
+
+
+  network {
+    network_id = hcloud_network.hcloud-network.id
+    ip         = "172.21.0.11"
+  }
+
+  firewall_ids = [
+    hcloud_firewall.allow-ssh.id,
+  ]
+
+  depends_on = [
+    hcloud_network_subnet.hcloud-network-subnet-1,
+  ]
+  labels = {
+    group = "lester"
+  }
+
+}
+
