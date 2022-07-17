77d779fc4f98097c9bb50f83451b710a36ffc49c
danielpodwysocki <daniel@dapo.tech>
2022-07-17 15:26:24 +0200

    add storage for the  instance

---
+++ hetzner-dev/storage.tf
@@ -0,0 +1,15 @@
+resource "hcloud_volume" "xardas_storage" {
+  name     = "xardas_vol1"
+  size     = 10
+  location = "fsn1"
+  count    = 1
+
+}
+
+resource "hcloud_volume_attachment" "xardas_storage" {
+  volume_id = hcloud_volume.xardas_storage[0].id
+  server_id = hcloud_server.xardas[0].id
+  count       = var.deploy ? 1 : 0
+  automount = false
+}
+
