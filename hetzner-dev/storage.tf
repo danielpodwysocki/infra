39d31d1ccd207e3ac78d5ae478c4f0424bfcab2a
danielpodwysocki <daniel@dapo.tech>
2022-07-19 20:06:15 +0200

    style: terraform fmt

--- hetzner-dev/storage.tf
+++ hetzner-dev/storage.tf
@@ -9,7 +9,7 @@ resource "hcloud_volume" "xardas_storage" {
 resource "hcloud_volume_attachment" "xardas_storage" {
   volume_id = hcloud_volume.xardas_storage[0].id
   server_id = hcloud_server.xardas[0].id
-  count       = var.deploy ? 1 : 0
+  count     = var.deploy ? 1 : 0
   automount = false
 }
 
